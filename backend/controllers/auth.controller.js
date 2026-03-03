a1f69c8423e29a615f255813c1f307371b9213bc
Pranav Cherkar <pranavcherkar.2002@gmail.com>
2026-03-03 21:09:07 +0530

    production issues

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -35,8 +35,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // true in production (https)
-      sameSite: "lax",
+      secure: true, // true in production (https)
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
