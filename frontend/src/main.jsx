487a0bd6248780419e9564958c798d3cfda527c6
Pranav Cherkar <pranavcherkar.2002@gmail.com>
2026-03-04 06:40:34 +0530

    fixed the refresh problem

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Buffer } from "buffer";
+window.global = window;
+window.Buffer = window.Buffer || Buffer;
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
